b63bbfe2d50432ae81727c79d07cfb2eb3dcfeb4
Marco <marco1@live.de>
2014-06-20 19:37:41 +0200

    Improved time indication in office

--- Website/index.php
+++ Website/index.php
@@ -139,7 +139,7 @@ if ($cookie_team != '__'.$cookie_id) {
 		}
 		?>
 	</a></td></tr>
-	<tr><td colspan="2"><?php echo '<img src="/images/clock.gif"> Saison '.$cookie_saison.' &middot; Spieltag '.$cookie_spieltag.' &middot; '.date('d.m.Y, H:i', time()).' Uhr'; ?></td></tr>
+	<tr><td colspan="2"><?php echo '<img style="vertical-align: middle;" alt="Systemzeit" src="/images/clock.gif" width="16" /> Saison '.$cookie_saison.' &middot; Spieltag '.$cookie_spieltag.' &middot; '.date('d.m.Y, H:i', time()).' Uhr'; ?></td></tr>
 	</tbody>
 	</table>
 	<?php

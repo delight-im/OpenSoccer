7ad6b13e7fb894eec8a1326b675d16d69ecddf6a
cosme12 <aaadiego502@gmail.com>
2014-06-24 17:27:59 -0300

    Update manager_kontaktlink.php

--- Website/manager_kontaktlink.php
+++ Website/manager_kontaktlink.php
@@ -6,18 +6,18 @@ $kontakt2 = mysql_query($kontakt1);
 if ($kontakt2 == FALSE) { exit; }
 $kontakt2a = mysql_num_rows($kontakt2);
 if ($kontakt2a == 0) {
-    $kontakt_link1 = '<tr class="odd"><td colspan="2" class="link"><a href="/freunde_aktion.php?id='.trim($_GET['id']).'&amp;aktion=einladen"'.noDemoClick($cookie_id).'>Freundschaft anbieten</a></td></tr>';
-	$kontakt_link2 = '<tr><td colspan="2" class="link"><a href="/freunde_aktion.php?id='.trim($_GET['id']).'&amp;aktion=Ignorieren"'.noDemoClick($cookie_id).'>Diesen User ignorieren</a></td></tr>';
+    $kontakt_link1 = '<tr class="odd"><td colspan="2" class="link"><a href="/freunde_aktion.php?id='.trim($_GET['id']).'&amp;aktion=einladen"'.noDemoClick($cookie_id).'><img width="16" style="vertical-align: middle;" alt="freunde" src="images/user_add.png"> Freundschaft anbieten</a></td></tr>';
+	$kontakt_link2 = '<tr><td colspan="2" class="link"><a href="/freunde_aktion.php?id='.trim($_GET['id']).'&amp;aktion=Ignorieren"'.noDemoClick($cookie_id).'><img width="16" style="vertical-align: middle;" alt="block" src="images/user_delete.png"> Diesen User ignorieren</a></td></tr>';
 }
 else {
 	$kontakt3 = mysql_fetch_assoc($kontakt2);
 	if ($kontakt3['typ'] == 'F') {
-		$kontakt_link1 = '<tr class="odd"><td colspan="2" class="link"><a href="/freunde_aktion.php?id='.trim($_GET['id']).'&amp;aktion=beenden"'.noDemoClick($cookie_id).'>Freundschaft beenden</a></td></tr>';
-		$kontakt_link2 = '<tr><td colspan="2" class="link"><a href="/freunde_aktion.php?id='.trim($_GET['id']).'&amp;aktion=Ignorieren"'.noDemoClick($cookie_id).'>Diesen User ignorieren</a></td></tr>';
+		$kontakt_link1 = '<tr class="odd"><td colspan="2" class="link"><a href="/freunde_aktion.php?id='.trim($_GET['id']).'&amp;aktion=beenden"'.noDemoClick($cookie_id).'><img width="16" style="vertical-align: middle;" alt="freunde" src="images/user_add.png"> Freundschaft beenden</a></td></tr>';
+		$kontakt_link2 = '<tr><td colspan="2" class="link"><a href="/freunde_aktion.php?id='.trim($_GET['id']).'&amp;aktion=Ignorieren"'.noDemoClick($cookie_id).'><img width="16" style="vertical-align: middle;" alt="block" src="images/user_delete.png"> Diesen User ignorieren</a></td></tr>';
 	}
 	elseif ($kontakt3['typ'] == 'B') {
-		$kontakt_link1 = '<tr class="odd"><td colspan="2" class="link"><a href="/freunde_aktion.php?id='.trim($_GET['id']).'&amp;aktion=einladen"'.noDemoClick($cookie_id).'>Freundschaft anbieten</a></td></tr>';
-		$kontakt_link2 = '<tr><td colspan="2" class="link"><a href="/freunde_aktion.php?id='.trim($_GET['id']).'&amp;aktion=StopBlock"'.noDemoClick($cookie_id).'>Diesen User nicht mehr ignorieren</a></td></tr>';
+		$kontakt_link1 = '<tr class="odd"><td colspan="2" class="link"><a href="/freunde_aktion.php?id='.trim($_GET['id']).'&amp;aktion=einladen"'.noDemoClick($cookie_id).'><img width="16" style="vertical-align: middle;" alt="freunde" src="images/user_add.png"> Freundschaft anbieten</a></td></tr>';
+		$kontakt_link2 = '<tr><td colspan="2" class="link"><a href="/freunde_aktion.php?id='.trim($_GET['id']).'&amp;aktion=StopBlock"'.noDemoClick($cookie_id).'><img width="16" style="vertical-align: middle;" alt="block" src="images/user_delete.png"> Diesen User nicht mehr ignorieren</a></td></tr>';
 	}
 }
 $kanfrage1 = "SELECT COUNT(*) FROM ".$prefix."freunde_anfragen WHERE (von = '".$cookie_id."' AND an = '".mysql_real_escape_string(trim($_GET['id']))."') OR (von = '".mysql_real_escape_string(trim($_GET['id']))."' AND an = '".$cookie_id."')";

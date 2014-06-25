1b9d8d8c0d9a0919e03d8b4d0f0dacd7af586371
cosme12 <aaadiego502@gmail.com>
2014-06-25 01:03:30 -0300

    Update geloeschterAccount.php

--- Website/geloeschterAccount.php
+++ Website/geloeschterAccount.php
@@ -1,6 +1,6 @@
 <?php include 'zz1.php'; ?>
-<title>Gelöschter Account | Ballmanager.de</title>
+<title><?php echo _('Gelöschter Account'); ?> | Ballmanager.de</title>
 <?php include 'zz2.php'; ?>
-<h1>Gelöschter Account</h1>
-<p>Du hast Deinen Account beim Ballmanager gelöscht. Deshalb kannst Du leider nicht mehr mitspielen.</p>
+<h1><?php echo _('Gelöschter Account'); ?></h1>
+<p><?php echo _('Du hast Deinen Account beim Ballmanager gelöscht. Deshalb kannst Du leider nicht mehr mitspielen.'); ?></p>
 <?php include 'zz3.php'; ?>

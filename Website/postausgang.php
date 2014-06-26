1f635905dde7507b9343901966155c9472c40040
cosme12 <aaadiego502@gmail.com>
2014-06-26 14:12:46 -0300

    Update postausgang.php

--- Website/postausgang.php
+++ Website/postausgang.php
@@ -1,5 +1,5 @@
 <?php include 'zz1.php'; ?>
-<title>Postausgang | Ballmanager.de</title>
+<title><?php echo _('Postausgang'); ?> | Ballmanager.de</title>
 <script type="text/javascript">
 function checkAll(quelle) {
 	for (i = 0; i < document.forms[0].length; i++) {
@@ -13,7 +13,7 @@ function checkAll(quelle) {
 </script>
 <?php include 'zz2.php'; ?>
 <h1>Postausgang</h1>
-<?php if ($loggedin == 0) { echo '<p>Du musst angemeldet sein, um diese Seite aufrufen zu können!</p>'; } else { ?>
+<?php if ($loggedin == 0) { echo '<p>'._('Du musst angemeldet sein, um diese Seite aufrufen zu können!').'</p>'; } else { ?>
 <?php
 if (isset($_POST['auswahl']) && $cookie_id != DEMO_USER_ID) {
 	if (is_array($_POST['auswahl'])) {
@@ -27,16 +27,16 @@ if (isset($_POST['auswahl']) && $cookie_id != DEMO_USER_ID) {
 	}
 }
 ?>
-<p style="text-align:right;"><a class="pagenava" href="/posteingang.php">Posteingang</a> <a class="pagenava aktiv" href="/postausgang.php">Postausgang</a></p>
+<p style="text-align:right;"><a class="pagenava" href="/posteingang.php"><?php echo _('Posteingang'); ?></a> <a class="pagenava aktiv" href="/postausgang.php"><?php echo _('Postausgang'); ?></a></p>
 <form action="/postausgang.php" name="checkBoxForm" method="post" accept-charset="utf-8">
 <p>
 <table>
 <thead>
 <tr class="odd">
 <th scope="col"><input type="checkbox" name="checkUncheckAll" onclick="checkAll(this);" /></th>
-<th scope="col">Betreff</th>
-<th scope="col">Empfänger</th>
-<th scope="col">Datum</th>
+<th scope="col"><?php echo _('Betreff'); ?></th>
+<th scope="col"><?php echo _('Empfänger'); ?></th>
+<th scope="col"><?php echo _('Datum'); ?></th>
 </tr>
 </thead>
 <tbody>
@@ -72,7 +72,7 @@ while ($sql3 = mysql_fetch_assoc($sql2)) {
 </table>
 </p>
 <p><select name="markedAction" size="1" style="width:200px">
-	<option value="DEL">Markierte löschen</option>
+	<option value="DEL"><?php echo _('Markierte löschen'); ?></option>
 </select></p>
 <p><input type="submit" value="Ausführen" onclick="return<?php echo noDemoClick($cookie_id, TRUE); ?> confirm('Bist Du sicher?');" /></p>
 </form>
@@ -80,8 +80,8 @@ while ($sql3 = mysql_fetch_assoc($sql2)) {
 echo '<div class="pagebar">';
 $wieviel_seiten = $blaetter3/$eintraege_pro_seite; // ERMITTELN DER SEITENANZAHL FÜR DAS INHALTSVERZEICHNIS
 $vorherige = $seite-1;
-if ($wieviel_seiten > 0) { echo '<a href="'.$_SERVER['SCRIPT_NAME'].'?seite=1">Erste</a> '; } else { echo '<span class="this-page">Erste</span>'; }
-if ($seite > 1) { echo '<a href="'.$_SERVER['SCRIPT_NAME'].'?seite='.$vorherige.'">Vorherige</a> '; } else { echo '<span class="this-page">Vorherige</span> '; }
+if ($wieviel_seiten > 0) { echo '<a href="'.$_SERVER['SCRIPT_NAME'].'?seite=1">'._('Erste').'</a> '; } else { echo '<span class="this-page">'._('Erste').'</span>'; }
+if ($seite > 1) { echo '<a href="'.$_SERVER['SCRIPT_NAME'].'?seite='.$vorherige.'">'._('Vorherige').'</a> '; } else { echo '<span class="this-page">'._('Vorherige').'</span> '; }
 $naechste = $seite+1;
 $vor4 = $seite-4; if ($vor4 > 0) { echo '<a href="'.$_SERVER['SCRIPT_NAME'].'?seite='.$vor4.'">'.$vor4.'</a> '; }
 $vor3 = $seite-3; if ($vor3 > 0) { echo '<a href="'.$_SERVER['SCRIPT_NAME'].'?seite='.$vor3.'">'.$vor3.'</a> '; }
@@ -92,8 +92,8 @@ $nach1 = $seite+1; if ($nach1 < $wieviel_seiten+1) { echo '<a href="'.$_SERVER['
 $nach2 = $seite+2; if ($nach2 < $wieviel_seiten+1) { echo '<a href="'.$_SERVER['SCRIPT_NAME'].'?seite='.$nach2.'">'.$nach2.'</a> '; }
 $nach3 = $seite+3; if ($nach3 < $wieviel_seiten+1) { echo '<a href="'.$_SERVER['SCRIPT_NAME'].'?seite='.$nach3.'">'.$nach3.'</a> '; }
 $nach4 = $seite+4; if ($nach4 < $wieviel_seiten+1) { echo '<a href="'.$_SERVER['SCRIPT_NAME'].'?seite='.$nach4.'">'.$nach4.'</a> '; }
-if ($seite < $wieviel_seiten) { echo '<a href="'.$_SERVER['SCRIPT_NAME'].'?seite='.$naechste.'">Nächste</a> '; } else { echo '<span class="this-page">Nächste</span> '; }
-if ($wieviel_seiten > 0) { echo '<a href="'.$_SERVER['SCRIPT_NAME'].'?seite='.ceil($wieviel_seiten).'">Letzte</a>'; } else { echo '<span clss="this-page">Letzte</span>'; }
+if ($seite < $wieviel_seiten) { echo '<a href="'.$_SERVER['SCRIPT_NAME'].'?seite='.$naechste.'">'._('Nächste').'</a> '; } else { echo '<span class="this-page">'._('Nächste').'</span> '; }
+if ($wieviel_seiten > 0) { echo '<a href="'.$_SERVER['SCRIPT_NAME'].'?seite='.ceil($wieviel_seiten).'">'._('Letzte').'</a>'; } else { echo '<span clss="this-page">'._('Letzte').'</span>'; }
 echo '</div>';
 ?>
 <?php } ?>

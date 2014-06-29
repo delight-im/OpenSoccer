f3449daccdba2c2cd2ec051149632e1f032a4f3d
cosme12 <aaadiego502@gmail.com>
2014-06-29 00:57:51 -0300

    Update ipInfo.php

--- Website/ipInfo.php
+++ Website/ipInfo.php
@@ -1,19 +1,19 @@
 <?php include 'zz1.php'; ?>
-<title>IP-Info | Ballmanager.de</title>
+<title><?php echo _('IP-Info'); ?> | Ballmanager.de</title>
 <?php include 'zz2.php'; ?>
 <?php
 if ($loggedin == 1) {
 if ($_SESSION['status'] != 'Helfer' && $_SESSION['status'] != 'Admin') { exit; }
 if (!isset($_GET['ip'])) { exit; }
 $getIP = mysql_real_escape_string(trim(strip_tags($_GET['ip'])));
-echo '<h1>IP-Info: '.$getIP.'</h1>';
-echo '<p>Die folgende Liste zeigt, welche User schon mit der IP-Adresse &quot;'.$getIP.'&quot; eingeloggt waren.</p>';
+echo '<h1>'._('IP-Info:').' '.$getIP.'</h1>';
+echo '<p>'._('Die folgende Liste zeigt, welche User schon mit der IP-Adresse').' &quot;'.$getIP.'&quot; '._('eingeloggt waren.').'</p>';
 ?>
 <table>
 <thead>
 <tr class="odd">
-<th scope="col">User</th>
-<th scope="col">Letzter Login</th>
+<th scope="col"><?php echo _('User'); ?></th>
+<th scope="col"><?php echo _('Letzter Login'); ?></th>
 </tr>
 </thead>
 <tbody>
@@ -39,7 +39,7 @@ while ($sql3 = mysql_fetch_assoc($sql2)) {
 </tbody>
 </table>
 <?php } else { ?>
-<h1>IP-Info</h1>
-<p>Du musst angemeldet sein, um diese Seite aufrufen zu können!</p>
+<h1><?php echo _('IP-Info'); ?></h1>
+<p><?php echo _('Du musst angemeldet sein, um diese Seite aufrufen zu können!'); ?></p>
 <?php } ?>
 <?php include 'zz3.php'; ?>

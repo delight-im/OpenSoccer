240f68eae8c906fb7275ed141ffe5cb9ccd348f9
cosme12 <aaadiego502@gmail.com>
2014-07-18 18:12:56 -0300

    Missing gettext tags

--- Website/freunde.php
+++ Website/freunde.php
@@ -8,7 +8,7 @@ $an2 = mysql_query($an1);
 $an2a = mysql_num_rows($an2);
 if ($an2a > 0) { echo '<h1>'._('Anfragen').'</h1><p><table><thead><tr class="odd"><th scope="col">'._('Manager').'</th><th scope="col">'._('Aktionen').'</th></tr></thead><tbody>';
     while ($an3 = mysql_fetch_assoc($an2)) {
-        echo '<tr><td class="link">'.displayUsername($an3['username'], $an3['von']).'</td><td><form action="/freunde_aktion.php" method="get" accept-charset="utf-8"><input type="hidden" name="id" value="'.$an3['von'].'" /><input type="submit" name="aktion" value="Annehmen"'.noDemoClick($cookie_id).' /> <input type="submit" name="aktion" value="Ablehnen"'.noDemoClick($cookie_id).' /></form></td>';
+        echo '<tr><td class="link">'.displayUsername($an3['username'], $an3['von']).'</td><td><form action="/freunde_aktion.php" method="get" accept-charset="utf-8"><input type="hidden" name="id" value="'.$an3['von'].'" /><input type="submit" name="aktion" value="'._('Annehmen').'"'.noDemoClick($cookie_id).' /> <input type="submit" name="aktion" value="'._('Ablehnen').'"'.noDemoClick($cookie_id).' /></form></td>';
     }
     echo '</tbody></table></p>';
 }
@@ -25,7 +25,7 @@ $kontakt1 = "SELECT username, last_login, f2, sortOrder FROM ".$prefix."freunde 
 $kontakt2 = mysql_query($kontakt1);
 $kontakt2a = mysql_num_rows($kontakt2);
 if ($kontakt2a == 0) { $kontakt2a = 'noch keine'; }
-echo '<h1>Du hast zurzeit '.$kontakt2a.' Freunde beim Ballmanager</h1>';
+echo '<h1>'.__('Du hast zurzeit %d Freunde beim Ballmanager', $kontakt2a).'</h1>';
 if ($kontakt2a == 'noch keine') { echo '<p>'._('Wenn Du Deine Freunde zu dieser Liste hinzufügen möchtest, dann öffne bitte das Profil des jeweiligen Managers und klicke unten auf den Link &quot;Freundschaft anbieten&quot;.').'</p>'; }
 else {
 	echo '<p><table><thead><tr class="odd"><th scope="col">'._('Manager').'</th><th scope="col">'._('Letzte Aktion').'</th><th scope="col">&nbsp;</th><th scope="col">&nbsp;</th></tr></thead><tbody>';
@@ -35,7 +35,7 @@ else {
 		$sortDown = $kontakt3['sortOrder']-1;
 		if ($sortDown < -125) { $sortDown = -125; }
         echo '<tr><td class="link">'.displayUsername($kontakt3['username'], $kontakt3['f2']).'</td><td>'.date('d.m.Y, H:i', $kontakt3['last_login']).' Uhr</td><td class="link"><a href="/post_schreiben.php?id='.$kontakt3['f2'].'">Nachricht schicken</a></td>';
-		echo '<td><a href="/freunde.php?sort='.$sortUp.'&amp;user='.$kontakt3['f2'].'"><img src="/images/arrow_up.png" alt="+" title="Nach oben verschieben" /></a> <a href="/freunde.php?sort='.$sortDown.'&amp;user='.$kontakt3['f2'].'"><img src="/images/arrow_down.png" alt="-" title="Nach unten verschieben" /></a></td>';
+		echo '<td><a href="/freunde.php?sort='.$sortUp.'&amp;user='.$kontakt3['f2'].'"><img src="/images/arrow_up.png" alt="+" title="'._('Nach oben verschieben').'" /></a> <a href="/freunde.php?sort='.$sortDown.'&amp;user='.$kontakt3['f2'].'"><img src="/images/arrow_down.png" alt="-" title="'._('Nach unten verschieben').'" /></a></td>';
 		echo '</tr>';
     }
     echo '</tbody></table></p>';
@@ -46,7 +46,7 @@ $kontakt1 = "SELECT username, last_login, f2, sortOrder FROM ".$prefix."freunde 
 $kontakt2 = mysql_query($kontakt1);
 $kontakt2a = mysql_num_rows($kontakt2);
 if ($kontakt2a == 0) { $kontakt2a = 'noch keine'; }
-echo '<h1>Du ignorierst zurzeit '.$kontakt2a.' User beim Ballmanager</h1>';
+echo '<h1>'._('Du ignorierst zurzeit %d User beim Ballmanager', $kontakt2a).'</h1>';
 if ($kontakt2a == 'noch keine') { echo '<p>'._('Wenn Du User zu dieser Liste hinzufügen möchtest, dann öffne bitte das Profil des jeweiligen Managers und klicke unten auf den Link &quot;Diesen User ignorieren&quot;.').'</p>'; }
 else {
 	echo '<p><table><thead><tr class="odd"><th scope="col">'._('Manager').'</th><th scope="col">'._('Letzte Aktion').'</th><th scope="col">&nbsp;</th><th scope="col">&nbsp;</th></tr></thead><tbody>';
@@ -56,7 +56,7 @@ else {
 		$sortDown = $kontakt3['sortOrder']-1;
 		if ($sortDown < -125) { $sortDown = -125; }
         echo '<tr><td class="link">'.displayUsername($kontakt3['username'], $kontakt3['f2']).'</td><td>'.date('d.m.Y, H:i', $kontakt3['last_login']).' Uhr</td><td class="link"><a href="/post_schreiben.php?id='.$kontakt3['f2'].'">Nachricht schicken</a></td>';
-		echo '<td><a href="/freunde.php?sort='.$sortUp.'&amp;user='.$kontakt3['f2'].'"><img src="/images/arrow_up.png" alt="+" title="Nach oben verschieben" /></a> <a href="/freunde.php?sort='.$sortDown.'&amp;user='.$kontakt3['f2'].'"><img src="/images/arrow_down.png" alt="-" title="Nach unten verschieben" /></a></td>';
+		echo '<td><a href="/freunde.php?sort='.$sortUp.'&amp;user='.$kontakt3['f2'].'"><img src="/images/arrow_up.png" alt="+" title="'._('Nach oben verschieben').'" /></a> <a href="/freunde.php?sort='.$sortDown.'&amp;user='.$kontakt3['f2'].'"><img src="/images/arrow_down.png" alt="-" title="'._('Nach unten verschieben').'" /></a></td>';
 		echo '</tr>';
     }
     echo '</tbody></table></p>';

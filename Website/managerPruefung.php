4967d5f69a8c8f30cb5686fceca1b69f232ab6a5
Marco <marco1@live.de>
2014-07-25 23:38:23 +0200

    Added gettext to managerPruefung.php

--- Website/managerPruefung.php
+++ Website/managerPruefung.php
@@ -1,5 +1,5 @@
 <?php include 'zz1.php'; ?>
-<title>Manager-Prüfung | Ballmanager.de</title>
+<title><?php echo _('Manager-Prüfung'); ?> | Ballmanager.de</title>
 <?php include 'zz2.php'; ?>
 <?php if ($loggedin == 1) { ?>
 <?php
@@ -8,10 +8,10 @@ function showErfolg($erfolg = 0) {
 	global $nDone;
 	if ($erfolg > 0) {
 		$nDone++;
-		return '<img src="/images/erfolg.png" width="16" alt="J" title="Erfolgreich bestanden" />';
+		return '<img src="/images/erfolg.png" width="16" alt="X" title="'._('Erfolgreich bestanden').'" />';
 	}
 	else {
-		return '<img src="/images/fehler.png" width="16" alt="N" title="Noch nicht abgeschlossen" />';
+		return '<img src="/images/fehler.png" width="16" alt="O" title="'._('Noch nicht abgeschlossen').'" />';
 	}
 }
 // DABEIBLEIBEN - PRÜFEN ANFANG
@@ -22,11 +22,11 @@ if ($stayed3 >= 1) {
 	setTaskDone('stay_time');
 }
 // DABEIBLEIBEN - PRÜFEN ENDE
-echo '<h1>Manager-Prüfung</h1>';
-echo '<p>Der Vorstand Deines Klubs hat viel Vertrauen in Dich. Aber bevor Du Transfers aushandeln darfst, sollst Du erst alle Aufgaben dieser Manager-Prüfung erfolgreich abschließen.</p>';
-$html = '<table><thead><tr class="odd"><th scope="col">&nbsp;</th><th scope="col">Prüfung</th></tr></thead><tbody>';
-$html .= '<tr><td>'.showErfolg(1).'</td><td>[01] Registriere Dich beim Ballmanager und aktiviere Deinen Account.</td></tr>';
-$html .= '<tr class="odd"><td>'.showErfolg(1).'</td><td>[02] Wähle nach dem ersten Login Dein Team und Deine Liga.</td></tr>';
+echo '<h1>'._('Manager-Prüfung').'</h1>';
+echo '<p>'._('Der Vorstand Deines Klubs hat viel Vertrauen in Dich. Aber bevor Du Transfers aushandeln darfst, sollst Du erst alle Aufgaben dieser Manager-Prüfung erfolgreich abschließen.').'</p>';
+$html = '<table><thead><tr class="odd"><th scope="col">&nbsp;</th><th scope="col">'._('Prüfung').'</th></tr></thead><tbody>';
+$html .= '<tr><td>'.showErfolg(1).'</td><td>[01] '._('Registriere Dich beim Ballmanager und aktiviere Deinen Account.').'</td></tr>';
+$html .= '<tr class="odd"><td>'.showErfolg(1).'</td><td>[02] '._('Wähle nach dem ersten Login Dein Team und Deine Liga.').'</td></tr>';
 $sql1 = "SELECT a.task, b.id AS done FROM ".$prefix."licenseTasks AS a LEFT JOIN ".$prefix."licenseTasks_Completed AS b ON a.shortName = b.task AND b.user = '".$cookie_id."'";
 $sql2 = mysql_query($sql1);
 $rowCounter = 2;
@@ -41,7 +41,7 @@ while ($sql3 = mysql_fetch_assoc($sql2)) {
 $html .= '</tbody></table>';
 $pDone = floor($nDone/$rowCounter*100);
 echo '<div style="width:400px; height:40px; margin:10px auto; border:1px solid #000; background-color:#fff; color:#fff"><div style="width:'.floor(400*$pDone/100).'px; height:40px; margin:0; background-color:#3b69b6;"></div></div>';
-echo '<p style="text-align:center; font-size:14px">'.$pDone.'% ABGESCHLOSSEN</p>';
+echo '<p style="text-align:center; font-size:14px">'.$pDone.'% <span style="text-transform:uppercase;">'._('Abgeschlossen').'</span></p>';
 if ($pDone >= 100) {
 	// GEGEBENENFALLS REFERRAL-BONUS AN WERBER VERGEBEN ANFANG
 	$werberData1 = "SELECT b.ids, b.team FROM ".$prefix."referrals AS a JOIN ".$prefix."users AS b ON a.werber = b.ids WHERE a.geworben = '".$cookie_id."'";
@@ -75,6 +75,6 @@ if ($pDone >= 100) {
 echo $html;
 ?>
 <?php } else { ?>
-<p>Du musst angemeldet sein, um diese Seite aufrufen zu können!</p>
+<p><?php echo _('Du musst angemeldet sein, um diese Seite aufrufen zu können!'); ?></p>
 <?php } ?>
 <?php include 'zz3.php'; ?>

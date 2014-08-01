44f7032e74f84b0035f577eeedac8bc3be94d3b6
Marco <marco1@live.de>
2014-08-01 18:25:15 +0200

    Tell search engines not to follow language selection links

--- Website/zz3.php
+++ Website/zz3.php
@@ -4,7 +4,7 @@
 </div>
 <?php echo showInfoBox($showInfoBox); /* Meldungen ausgeben */ ?>
 <div><span id="rfooter" style="color:#666; width:820px; margin-left:auto; margin-right:auto; height:55px; text-align:center; font-size:80%; text-decoration:none">
-    <span title="<?php echo I18N::getBrowserLanguage(); ?>"><?php echo _('Sprache:'); ?></span> <a href="/?setLocale=de_DE">Deutsch</a> &middot; <a href="/?setLocale=en_US">English</a> &middot; <a href="/?setLocale=es_ES">Español</a><br />
+    <span title="<?php echo I18N::getBrowserLanguage(); ?>"><?php echo _('Sprache:'); ?></span> <a rel="nofollow" href="/?setLocale=de_DE">Deutsch</a> &middot; <a rel="nofollow" href="/?setLocale=en_US">English</a> &middot; <a rel="nofollow" href="/?setLocale=es_ES">Español</a><br />
 	<a href="/regeln.php#regeln" rel="nofollow"><?php echo _('Regeln'); ?></a> &middot;
     <a href="/android_app.php"><?php echo _('Android-App'); ?></a> &middot;
 	<a href="/impressum.php" rel="nofollow"><?php echo _('Impressum'); ?></a> &middot;

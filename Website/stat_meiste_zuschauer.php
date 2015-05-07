3e1838b29ea4d0729f77a647f1ed20a5b5717400
cosme12 <aaadiego502@gmail.com>
2015-05-07 15:10:30 -0300

    Missing gettext tags

--- Website/stat_meiste_zuschauer.php
+++ Website/stat_meiste_zuschauer.php
@@ -36,7 +36,7 @@
     }
     ?>
 </select>
-<input type="submit" value="Auswählen" /></p>
+<input type="submit" value="<?php echo _('Auswählen'); ?>" /></p>
 </form>
 <h1><?php echo _('Meiste Zuschauer'); ?></h1>
 <p><?php echo _('Hier sind die Teams mit dem höchsten Zuschauerschnitt in der aktuellen Saison aufgelistet.'); ?></p>

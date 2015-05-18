e6dfc2c297edc675cac3f57ba80346b914b930b3
cosme12 <aaadiego502@gmail.com>
2015-05-17 23:48:26 -0300

    Missing gettext tags

--- Website/stat_wertvollste_teams.php
+++ Website/stat_wertvollste_teams.php
@@ -36,7 +36,7 @@
     }
     ?>
 </select>
-<input type="submit" value="Auswählen" /></p>
+<input type="submit" value="<?php echo _('Auswählen'); ?>" /></p>
 </form>
 <h1><?php echo _('Wertvollste Teams'); ?></h1>
 <p><?php echo _('Der Marktwert eines Teams ist die Summer aller Marktwerte der einzelnen Spieler. In dieser Tabelle wurden alle Teams nach ihrem Marktwert sortiert aufgelistet.'); ?></p>
@@ -70,7 +70,7 @@ if ($temp == FALSE) {
     $counter = 1;
     while ($sql3 = mysql_fetch_assoc($sql2)) {
         if ($counter % 2 == 1) { $tmp_liga_cache .= '<tr class="team_'.$sql3['ids'].'">'; } else { $tmp_liga_cache .= '<tr class="team_'.$sql3['ids'].' odd">'; }
-        $tmp_liga_cache .= '<td>'.$counter.'.</td><td class="link"><a href="/team.php?id='.$sql3['ids'].'">'.$sql3['name'].'</a></td><td>'.number_format($sql3['teamwert'], 0, ',', '.').' €</td>';
+        $tmp_liga_cache .= '<td>'.$counter.'.</td><td class="link"><a href="/team.php?id='.$sql3['ids'].'">'.$sql3['name'].'</a></td><td>'__('€ %s', number_format($sql3['teamwert'], 0, ',', '.')).'</td>';
         $tmp_liga_cache .= '</tr>';
         $counter++;
     }

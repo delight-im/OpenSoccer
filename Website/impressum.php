9a05e87dadcb42492db54726be1e7d1ec9d28cfe
Marco <marco1@live.de>
2015-09-26 20:02:10 +0200

    Updated credits on public website

--- Website/impressum.php
+++ Website/impressum.php
@@ -3,8 +3,11 @@
 <?php include 'zz2.php'; ?>
 <h1><?php echo _('Impressum'); ?></h1>
 <?php echo CONFIG_CONTACT_PAGE_HTML; ?>
-<h1><?php echo _('Danke ...') ?></h1>
-<p>... an <a href="http://famfamfam.com/">famfamfam</a> für die <a href="http://famfamfam.com/lab/icons/silk/">&quot;Silk Icons&quot;</a></p>
-<p>... an <a href="http://famfamfam.com/">famfamfam</a> für die <a href="http://famfamfam.com/lab/icons/flags/">&quot;Flags&quot;</a></p>
-<p>... an <a href="http://prothemedesign.com/">Pro Theme Design</a> für die <a href="http://prothemedesign.com/free-webdesign-tools/circular-icons/">&quot;Circular Icons&quot;</a></p>
+<h1><?php echo _('Danksagungen, Quellen und Autoren') ?></h1>
+<ul>
+<li><?php echo _('Design'); ?> &mdash; <a href="http://www.styleshout.com/">Styleshout</a></li>
+<li><a href="http://famfamfam.com/lab/icons/silk/">&quot;Silk Icons&quot;</a> &mdash; <a href="http://famfamfam.com/">famfamfam</a></li>
+<li><a href="http://famfamfam.com/lab/icons/flags/">&quot;Flags&quot;</a> &mdash; <a href="http://famfamfam.com/">famfamfam</a></li>
+<li><a href="http://prothemedesign.com/free-webdesign-tools/circular-icons/">&quot;Circular Icons&quot;</a> &mdash; <a href="http://prothemedesign.com/">Pro Theme Design</a></li>
+</ul>
 <?php include 'zz3.php'; ?>
